6adb30903b89f493cabc71ebdb280c05cd21cc1f
nuclearrockstone <nuclearrockstone@gmail.com>
2024-12-18 15:10:13 +0800

    Add ColorPicker

--- app.py
+++ app.py
@@ -4,28 +4,33 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import TerminalFormatter
 from pygments.formatters import HtmlFormatter
 
-CODE_TEST = '''
+COLOR_BAR    ="rgb(171, 176, 182)"
+COLOR_WINDOW ="rgb(243, 243, 243)"
+
+CODE_TEST    = '''
 def greet(name):
     print(f'Hello, {name}!')
 greet('World')
 '''
-def code_count(line_code):
+def Code_Count(line_code):
     '''
     Count the num of line and max line
     '''
     lines = line_code.split('\n')
     return max(len(line) for line in lines)
 
-def wrap_code(code):
+def Wrap_Code(code,color_bar="rgb(171, 176, 182)",color_window="rgb(243, 243, 243)"):
+    print(3)
     formatter = HtmlFormatter(style='colorful', linenos=True,wrapcode=True)
-    MAX_CHAR = 15*code_count(code)
+    MAX_CHAR = 15*Code_Count(code)
+
     CUSTOM_HTML = """
-    <div style="width: {0}px; height: 40px; background-color: lightgray; border-radius: 1em 1em 0 0; display: flex; align-items: center; justify-content: start; gap: 10px;">
+    <div style="width: {0}px; height: 40px; background-color: {1}; border-radius: 1em 1em 0 0; display: flex; align-items: center; justify-content: start; gap: 10px;">
             <div style="width: 20px; height: 20px; background-color: red; border-radius: 50%; margin-left:10px"></div>
             <div style="width: 20px; height: 20px; background-color: green; border-radius: 50%;"></div>
             <div style="width: 20px; height: 20px; background-color: orange; border-radius: 50%;"></div>
     </div>
-    """.format(MAX_CHAR)
+    """.format(MAX_CHAR,color_bar)
     CUSTOM_CSS  = """
     .normal,.p,.nb,.n,.s1,.sa,.k,.nf{{ 
             font-size:30px;
@@ -36,11 +41,11 @@ def wrap_code(code):
     }}
     .highlight{{
             width: {0}px;
-            background-color: #eeeeee;
+            background-color: {1};
             border-bottom-left-radius: 1em;
             border-bottom-right-radius: 1em;   
     }}
-    """.format(MAX_CHAR)
+    """.format(MAX_CHAR,color_window)
 
     FORMAT_CSS = formatter.get_style_defs('.highlight')
     FORMAT_CODE = highlight(code, PythonLexer(), formatter)
@@ -55,17 +60,34 @@ def wrap_code(code):
     return FULL_HTML
 
 
+
 with gr.Blocks() as beautifulcode:
     with gr.Row():
-        """ gr.HTML("<h1>Hello</h1>")
-        gr.HTML("<h1>Hello</h1>", container=True)
-        gr.HTML("<h1>Hello</h1>", container=True, show_label=True) """
-        input = gr.Code(language="python")
+        with gr.Column():
+            input_code = gr.Code(language="python")
+            with gr.Group():
+                color_follow=gr.Checkbox()
+                cp_bar = gr.ColorPicker(value=COLOR_BAR)
+                cp_window = gr.ColorPicker(value=COLOR_WINDOW)                
+                def Cp_Bar_Fun(code,cp_bar,cp_window,flag):
+                    
+                    if flag:
+                        FULL_HTML = Wrap_Code(code,cp_bar,cp_bar)
+                        return FULL_HTML,gr.update(value=cp_bar)
+                    else:
+                        FULL_HTML = Wrap_Code(code,cp_bar,cp_window)
+                        return FULL_HTML,gr.update(value=cp_window)
+                def Cp_Window_Fun(code,cp_bar,cp_window,flag):
+                    
+                    if flag:
+                        FULL_HTML = Wrap_Code(code,cp_window,cp_window)
+                        return FULL_HTML,gr.update(value=cp_window)
+                    else:
+                        FULL_HTML = Wrap_Code(code,cp_bar,cp_window)
+                        return FULL_HTML,gr.update(value=cp_bar)               
         output = gr.HTML(container=True, show_label=True)
-        """ with gr.Column():
-            output1 = gr.Number(label="Row",show_label=True)
-            output2 = gr.Number(label="MaxChar",show_label=True)
-        output = [output1,output2] """
-        input.change(wrap_code,inputs=input,outputs=output)
+        input_code.change(Wrap_Code,inputs=[input_code,cp_bar,cp_window],outputs=output)
+        cp_bar.input(Cp_Bar_Fun,inputs=[input_code,cp_bar,cp_window,color_follow],outputs=[output,cp_window])
+        cp_window.input(Cp_Window_Fun,inputs=[input_code,cp_bar,cp_window,color_follow],outputs=[output,cp_bar])
 
 beautifulcode.launch()
--- app2.py
+++ app2.py
@@ -1,18 +1,36 @@
 import gradio as gr
-
+color1 = ""
+color2 = ""
 with gr.Blocks() as demo:
-    input_text = gr.Textbox(label="input")
-    mode = gr.Radio(["textbox", "button"], value="textbox")
-
-    @gr.render(inputs=[input_text, mode])
-    def show_split(text, mode):
-        if len(text) == 0:
-            gr.Markdown("## No Input Provided")
+    input = gr.Checkbox()
+    o1 = gr.ColorPicker(value=color1)
+    o2 = gr.ColorPicker(value=color2) 
+    @gr.render(inputs=input)
+    def show_split(check):
+        global color2
+        global color1
+        global o1
+        global o2       
+        def of1(input):
+            print(input)
+            return gr.update(value=input)
+        def of2(input):
+            print(input)
+            return gr.update(value=input)
+        def of11(input):
+            global color1 
+            color1 = input
+            return None
+        def of12(input):
+            global color2 
+            color2 = input
+            return None
+        if check:
+            o1.input(of1,o1,o2)
+            o2.input(of2,o2,o1)
         else:
-            for letter in text:
-                if mode == "textbox":
-                    gr.Textbox(letter)
-                else:
-                    gr.Button(letter)
+            o1.input(of11,o1)
+            o2.input(of12,o2)
+            pass
 
 demo.launch()
